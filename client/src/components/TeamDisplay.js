e9294bdf1a610f95645bc8622dcd7c9bf17cbbb5
Allan Elfe <aaelfe@crimson.ua.edu>
2021-07-14 15:58:47 -0500

    Random cleanup, end of day

--- client/src/components/TeamDisplay.js
+++ client/src/components/TeamDisplay.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Paper } from '@material-ui/core'
 
 const TeamDisplay = ({tournament}) => {
 
     //here need to get data about players from player/playerdisplay and put in array
 
     return (
-        <Grid container spacing={3}>
-            {/*here need to map player data array and create elements*/}
-        </Grid>
+        <Paper>
+            <Grid container spacing={3}>
+                {/*here need to map player data array and create elements*/}
+            </Grid>
+        </Paper>
     );
 }
 
