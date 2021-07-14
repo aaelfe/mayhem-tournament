ef00bd252a9a82e412abdf742be8c34168369303
Allan Elfe <aaelfe@crimson.ua.edu>
2021-07-14 11:01:40 -0500

    Connecting to Mongo Cluster, setup .env, updated .gitignore

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+//import routes
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+    .then(() => app.listen(PORT, () => console.log(`Server running, port ${PORT}`)))
+    .catch((err) => console.log(error));
+
+mongoose.set('useFindAndModify', false);
