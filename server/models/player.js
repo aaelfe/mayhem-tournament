48a578cd5189c90ffb66f7d906f742bf6370957a
Allan Elfe <aaelfe@crimson.ua.edu>
2021-07-14 11:15:29 -0500

    created mongoose Player model

--- server/models/player.js
+++ server/models/player.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 //create schema
+const playerSchema = mongoose.Schema({ name: String });
 
 //create model w schema
+const Player = mongoose.model('Player', playerSchema);
 
 //export model
+export default Player;
