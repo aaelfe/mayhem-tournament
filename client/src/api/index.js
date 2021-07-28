188c2da6d8714d05c32aa1243f4ae431293b7528
Allan Elfe <aaelfe@crimson.ua.edu>
2021-07-28 11:34:02 -0500

    Fixed SSL error on POST req (http rather than https in api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://localhost:5000/players';
+const url = 'http://localhost:5000/players';
 
 export const fetchPlayers = () => axios.get(url);
 export const createPlayer = (newPlayer) => {axios.post(url, newPlayer);console.log('createPlayer api called')};
