c6f9777d2e478ceaf2f4bc2a90d7007e50a2e80e
Allan Elfe <aaelfe@crimson.ua.edu>
2021-07-29 12:46:17 -0500

    Added api calls for tournament fetch, create, delete

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/players';
+const url = 'http://localhost:5000';
 
-export const fetchPlayers = () => axios.get(url);
-export const createPlayer = (newPlayer) => axios.post(url, newPlayer);
-export const deletePlayer = (_id) => axios.delete(`${url}/${_id}`);
+export const fetchPlayers = () => axios.get(`${url}/players`);
+export const createPlayer = (newPlayer) => axios.post(`${url}/players`, newPlayer);
+export const deletePlayer = (_id) => axios.delete(`${url}/players/${_id}`);
+
+export const fetchTournaments = () => axios.get(`${url}/tournaments`);
+export const createTournament = (newTournament) => axios.post(`${url}/tournaments`, newTournament);
+export const deleteTournament = (_id) => axios.delete(`${url}/tournaments/${_id}`);
